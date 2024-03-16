b61b9eccba9d65c9389ff187849da577dd1695c5
2814109 <2814109k@gmail.com>
2024-03-17 00:23:15 +0900

    add effect function

--- app/routes/sample.tsx
+++ app/routes/sample.tsx
@@ -1,7 +1,27 @@
-import { Console, Effect } from "effect";
+import { Console, Effect, pipe } from "effect";
+
+type TUser = {
+  name: string;
+  age: number;
+};
 export default function Index() {
-  const program = Console.log("hello");
-  const handleClick = () => Effect.runSync(program);
+  const user = {
+    name: "hello",
+    age: 20,
+  };
+  const program = pipe(user, validateUser);
+  const handleClick = () => {
+    const result = Effect.match(program, {
+      onSuccess: () => console.log("success"),
+      onFailure: (error) => window.alert(error),
+    });
+
+    return Effect.runSync(result);
+  };
 
   return <button onClick={handleClick}>sample</button>;
 }
+
+const validateUser = (user: TUser) => {
+  return user.age < 18 ? Effect.succeed("child") : Effect.fail("adult");
+};
