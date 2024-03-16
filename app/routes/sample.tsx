3cec50ffdd356a8d4e3a08ae8fd926d85fb6716e
2814109 <2814109k@gmail.com>
2024-03-17 01:26:02 +0900

    update

--- app/routes/sample.tsx
+++ app/routes/sample.tsx
@@ -1,3 +1,4 @@
+import { TRIPLE, user } from "~/constants";
 import { useCount } from "~/hooks/useCount";
 
 export default function Index() {
@@ -5,6 +6,8 @@ export default function Index() {
 
   return (
     <>
+      <p>init value is {JSON.stringify(user)}</p>
+      <p>multiplication : {TRIPLE}</p>
       <button onClick={handleClick}>GrowUp</button>
       <button onClick={handleReset}>Reset</button>
       <p>{state.count}</p>
