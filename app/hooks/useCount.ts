9173c21bf2b456ec2bddfb0c70c14919686ba625
2814109 <2814109k@gmail.com>
2024-03-17 14:12:28 +0900

    rename

--- app/hooks/useCount.ts
+++ app/hooks/useCount.ts
@@ -1,5 +1,5 @@
 import { useCallback, useReducer } from "react";
-import { Effect } from "effect";
+import { Effect as E } from "effect";
 import { reducer } from "~/libs/reducer";
 import { TRIPLE, user } from "~/constants";
 
@@ -7,20 +7,20 @@ export const useCount = () => {
   const [state, dispatch] = useReducer(reducer, user);
 
   const growUp = (multiplication: number) => (user: typeof state) =>
-    Effect.succeed(user.age * multiplication);
+    E.succeed(user.age * multiplication);
 
   const validateUser = (user: typeof state) =>
-    user.age < 18 ? Effect.succeed(user) : Effect.fail("adult");
+    user.age < 18 ? E.succeed(user) : E.fail("adult");
 
-  const program = Effect.succeed(user).pipe(
-    Effect.flatMap(validateUser),
-    Effect.flatMap(growUp(TRIPLE))
+  const program = E.succeed(user).pipe(
+    E.flatMap(validateUser),
+    E.flatMap(growUp(TRIPLE))
   );
 
   const handleClick = useCallback(
     () =>
-      Effect.runSync(
-        Effect.match(program, {
+      E.runSync(
+        E.match(program, {
           onSuccess: (success) =>
             dispatch({ type: "SET_COUNT", payload: success }),
           onFailure: (error) => window.alert(error),
